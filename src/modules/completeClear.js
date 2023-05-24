040ba24bbd264bfca9bbb513c2d03c43e23b2c39
coker2019 <olabisi2k11@gmail.com>
2023-05-24 11:31:39 +0100

    Add development.

--- src/modules/completeClear.js
+++
@@ -1,9 +0,0 @@
-import save from './Filter.js';
-
-const clearCompleted = (tasks) => {
-  tasks = tasks.filter((i) => !i.completed);
-  save(tasks);
-  document.location.reload();
-};
-
-export default clearCompleted;
